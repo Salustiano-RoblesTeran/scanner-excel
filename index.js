c85bbe98a2290e77038a81c80f7b7c99a58c926c
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-11 22:27:04 -0300

    terminamos de cruzar los datos

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const xlsx = require('xlsx');
 
 // Función para cargar y convertir el archivo CSV de transacciones
-const convertirTn = () => {
-    const excel = xlsx.readFile("./ventasTn.csv");
+const obtenerTn = () => {
+    const excel = xlsx.readFile("./ventas.csv");
     const nombreHoja = excel.SheetNames[0]; // Suponemos que hay una sola hoja en el archivo
 
     const datos = xlsx.utils.sheet_to_json(excel.Sheets[nombreHoja], {
@@ -10,20 +10,22 @@ const convertirTn = () => {
     });
 
     // Mapear los datos a un formato específico
-    const datosImp = datos.map((dato) => ({
-        'numero de orden': dato['Número de orden'],
-        'Estado del pago': dato['Estado del pago'],
-        'Nombre del comprador': dato['Nombre del comprador'],
-        'Medio de pago': dato['Medio de pago'],
-        'Notas del vendedor': dato['Notas del vendedor'],
-        'Identificador de la transacción en el medio de pago': dato['Identificador de la transacción en el medio de pago']
-    }));
+    const datosImp = datos
+        .filter(dato => dato['Estado del pago'] !== 'Pendiente' && dato['Estado del pago'] !== undefined)
+        .map(dato => ({
+            'Número de orden': dato['Número de orden'],
+            'Estado del pago': dato['Estado del pago'],
+            'Nombre del comprador': dato['Nombre del comprador'],
+            'Medio de pago': dato['Medio de pago'],
+            'Notas del vendedor': dato['Notas del vendedor'],
+            'Identificador de la transacción en el medio de pago': dato['Identificador de la transacción en el medio de pago']
+        }));
 
     return datosImp;
 }
 
 // Función para cargar y convertir el archivo CSV de Mercado Pago
-const convertirMp = () => {
+const obtenerMp = () => {
     const excel = xlsx.readFile("./ventasMp.csv");
     const nombreHoja = excel.SheetNames[0]; // Suponemos que hay una sola hoja en el archivo
 
@@ -33,52 +35,90 @@ const convertirMp = () => {
 
     // Mapear los datos a un formato específico
     const datosImpMp = datos.map((dato) => ({
-        'Número de operación de Mercado Pago': dato['NÃºmero de operaciÃ³n de Mercado Pago (operation_id)'],
-        'Valor del producto': dato['Valor del producto (transaction_amount)'],
-        'Tarifa de Mercado Pago': dato['Tarifa de Mercado Pago (mercadopago_fee)'],
-        'Comisión por uso de plataforma de terceros': dato['ComisiÃ³n por uso de plataforma de terceros (marketplace_fee)'],
-        'Monto recibido': dato['Monto recibido (net_received_amount)'],
-        'Cuotas (installments)': dato['Cuotas (installments)'],
-        'Costos de financiación (financing_fee)': dato['Costos de financiaciÃ³n (financing_fee)']
+        'Número de operación de Mercado Pago': Number(dato['NÃºmero de operaciÃ³n de Mercado Pago (operation_id)']),
+        'Valor del producto': Number(dato['Valor del producto (transaction_amount)']),
+        'Tarifa de Mercado Pago': Number(dato['Tarifa de Mercado Pago (mercadopago_fee)']),
+        'Comisión por uso de plataforma de terceros': Number(dato['ComisiÃ³n por uso de plataforma de terceros (marketplace_fee)']),
+        'Monto recibido': Number(dato['Monto recibido (net_received_amount)']),
+        'Cuotas (installments)': Number(dato['Cuotas (installments)']),
+        'Costos de financiación (financing_fee)': Number(dato['Costos de financiaciÃ³n (financing_fee)'])
     }));
 
     return datosImpMp;
 }
 
-// Función principal para comparar los datos
-const compararDatos = () => {
-    const datosImp = convertirTn();
-    const datosImpMp = convertirMp();
+// Función para revisar las "Notas del vendedor" y actualizar el identificador de la transacción en el medio de pago
+const eliminarNotas = () => {
+    const datoTn = obtenerTn();
 
-    // ver si hay contenido en las notas
-    const notas = datosImp.filter((dato)=> dato['Notas del vendedor'] !== undefined);
-    console.log(notas)
+    const regex = /\b\d{11}\b/;
 
-    // Recorrer los datos y comparar
-    datosImp.forEach((datoTn) => {
-        const idTransaccionTn = datoTn['Identificador de la transacción en el medio de pago'];
-        
-        // Buscar la correspondiente transacción en Mercado Pago
-        const datoMp = datosImpMp.find((dato) => {
-            const numeroOperacionMp = dato['Número de operación de Mercado Pago'];
-            return numeroOperacionMp === idTransaccionTn;
-        });
+    const datosActualizados = datoTn.map(dato => {
+        const notas = String(dato['Notas del vendedor']); // Asegurarse de que notas es un string
+        const match = notas.match(regex);
 
-        if (datoMp) {
-            // Aquí puedes realizar cualquier acción con los datos encontrados
-            console.log(`Coincidencia encontrada para ID: ${idTransaccionTn}`);
-            console.log('Datos de transacción en TN:', datoTn);
-            console.log('Datos de transacción en Mercado Pago:', datoMp);
-            console.log('---');
-        } else {
-            console.log(`No se encontró coincidencia para ID: ${idTransaccionTn}`);
+        if (match) {
+            dato['Identificador de la transacción en el medio de pago'] = match[0];
         }
+
+        return dato;
     });
+
+    return datosActualizados;
 }
 
+// Función para cruzar la información de ambos arreglos y guardarlos en un tercer arreglo definitivo
+const cruzarInfo = () => {
+    const datosActualizados = eliminarNotas();
+    const datosMp = obtenerMp();
 
-// Ejecutar la comparación
-compararDatos();
+    const datosCruzados = datosActualizados.map(dato => {
+        const identificador = parseInt(dato['Identificador de la transacción en el medio de pago'], 10);
 
+        const datosMpEncontrado = datosMp.find(mp => mp['Número de operación de Mercado Pago'] === identificador);
 
-let mp = 'https://www.mercadopago.com.ar/balance/reports/collection';
+        if (datosMpEncontrado) {
+            const valorProducto = Math.abs(datosMpEncontrado['Valor del producto']);
+            const tarifaMercadoPago = Math.abs(datosMpEncontrado['Tarifa de Mercado Pago']);
+            const comisionTerceros = Math.abs(datosMpEncontrado['Comisión por uso de plataforma de terceros']);
+            const costosFinanciacion = Math.abs(datosMpEncontrado['Costos de financiación (financing_fee)']);
+            const impuestoDebitoCredito = valorProducto * 0.006;
+            const sirtac = valorProducto * 0.001;
+            const montoRecibido = datosMpEncontrado['Monto recibido'];
+
+            // Calcular el Impuesto IIBB
+            const impuestoIIBB = valorProducto - montoRecibido - costosFinanciacion - tarifaMercadoPago - impuestoDebitoCredito - sirtac - comisionTerceros;
+
+            return {
+                'Pedido': dato['Número de orden'],
+                //'Estado del pago': dato['Estado del pago'],
+                'Nombre': dato['Nombre del comprador'],
+                'Medio de pago': dato['Medio de pago'],
+                //'Identificador de la transacción en el medio de pago': dato['Identificador de la transacción en el medio de pago'],
+                'Precio Venta': valorProducto,
+                'Cargo de Mercado Pago': tarifaMercadoPago,
+                'Comisión de terceros': comisionTerceros,
+                'Monto recibido': montoRecibido,
+                //'Cuotas (installments)': datosMpEncontrado['Cuotas (installments)'],
+                'Interes Cuota': costosFinanciacion,
+                'Impuesto IIBB regimen SIRTAC': sirtac,
+                'Impuesto debito/credito': impuestoDebitoCredito,
+                'Impuesto IIBB': impuestoIIBB,
+            };
+        } else {
+            return {
+                'Pedido': dato['Número de orden'],
+                'Estado del pago': dato['Estado del pago'],
+                'Nombre': dato['Nombre del comprador'],
+                'Medio de pago': dato['Medio de pago'],
+                'Identificador de la transacción en el medio de pago': dato['Identificador de la transacción en el medio de pago']
+            };
+        }
+    });
+
+    return datosCruzados;
+}
+
+// Ejemplo de uso
+const datosFinales = cruzarInfo();
+console.log(datosFinales);
